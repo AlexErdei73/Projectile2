c8e19f4e1becd10c6f53884cf0321632246e1571
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-12-26 00:01:38 +0000

    Make angleScrollbar work to input angle

--- src/ControlFrame.java
+++ src/ControlFrame.java
@@ -4,6 +4,7 @@ public class ControlFrame {
     ControlFrame() {
         Simulation projectile = new Simulation();
         Frame controlFrame = new Frame("Control Frame");
+        controlFrame.setLayout(new GridLayout(0, 1));
         controlFrame.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
@@ -12,15 +13,17 @@ public class ControlFrame {
             }
         });
         Panel controlPanel = new Panel();
+        DoubleScroller angleScroller = new DoubleScroller("Launch angle in degrees = ", 0, 90, 1, 45 );
         Button launchButton = new Button("Launch");
         launchButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                projectile.launch(0.05, 9.8995, 45);
+                projectile.launch(0.05, 9.8995, angleScroller.getValue());
             }
         });
         controlPanel.add(launchButton);
         controlFrame.add(controlPanel);
+        controlFrame.add(angleScroller);
         controlFrame.pack();
         controlFrame.setVisible(true);
     }
